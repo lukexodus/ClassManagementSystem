81db59719b1ffb516bcacce2430eb4c0e2edf32a
Adrian Labasan <zionexodus7@protonmail.com>
2024-05-04 16:47:23 +0800

    Create CMS.c

---
+++ CMS.c
@@ -0,0 +1,66 @@
+// Project Description
+// Class Management System
+// - Add student
+//   - student no./ID
+//   - student name
+//   - grades
+// - Display student record
+//   - grade for each subject
+//   - GWA
+// - Search
+//   - displays the student record
+//
+// Data stored to a text file (CSV)
+
+// A CSV (Comma-Separated Values) file is a simple text-based
+// file format used to store tabular data. In a CSV file, each
+// line represents a row of data, with fields separated by
+// commas (or other delimiters like semicolons or tabs). - ChatGPT
+
+int main()
+{
+    // The program has 4 phases.
+    // Phase 3 has 3 parts.
+    // So, all in all, there are 6 parts.
+    //
+    // [1] Creates the data file if it does not exist
+    //
+    //
+    // [2] Reads all students data from the data file
+    //  - Stores the data to an array.
+    //  - Tutorial: https://www.youtube.com/watch?v=rbVt5v8NNe8
+    //
+    //
+    // [3] Displays student records and options
+    //  - Use a loop for this part so that the program
+    //    continues until exited by the user.
+    //  - 4 options:
+    //    [1] Add Student
+    //    [2] Display student record
+    //    [3] Search
+    //    [4] Exit
+    //
+    //
+    // [3.A] Add Student Option
+    //  - Input student data and grades
+    //    - Last name, first name, ID
+    //    - MATH101, CMPSC113, COMM01, MATH01, NSTP02, MATH16, PATHFIT2 grades
+    //  - Computes the GWA
+    //  - Stores all of these to the student struct in the array
+    //
+    // [3.B] Display student record
+    //  - Make the user select one student
+    //  - and then displays the data and grades of the student
+    //
+    // [3.C] Search
+    //  - Let the user search for students
+    //    based on the keyword/s inputted
+    //  - If the search matches multiple students,
+    //    ask again which specific student whose
+    //    data and grades will be shown
+    //  - Displays the data and grades of the selected student
+    //
+    //
+    // [4] Stores data to the data back to the data file
+    //  - Tutorial: https://www.youtube.com/watch?v=7ZFgphYJvUA
+}
