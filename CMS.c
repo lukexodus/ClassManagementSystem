edec312e33a44ce23c70fcbc5871dbf1fb14cf6e
Natividad000 <signinplease143@gmail.com>
2024-05-16 20:12:21 +0800

    Update CMS.c

--- CMS.c
+++ CMS.c
@@ -16,7 +16,30 @@
 // file format used to store tabular data. In a CSV file, each
 // line represents a row of data, with fields separated by
 // commas (or other delimiters like semicolons or tabs). - ChatGPT
+#include <stdio.h>
+#include <stdlib.h>
 
+// Define the name of the data file
+#define dataFilePath"students.csv"
+
+// Function to create the data file if it does not exist
+void createdataFilePath() {
+    // Open the file in append mode. If it does not exist, it will be created.
+    FILE *file = fopen(dataFilePath, "a");
+    if (!file) { // Check if the file was opened successfully
+        printf("Error creating the data file.\n");
+        exit(EXIT_FAILURE); // Exit the program if the file could not be opened
+    }
+    fclose(file); // Close the file
+    printf("data file checked/created successfully.\n");
+}
+
+int main() {
+    // Create the data file if it does not exist
+    createdataFilePath();
+    return 0;
+}
+    
 #include <stdio.h>
 
 // To be able to use `bool` type
